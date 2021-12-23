7c1ad49658254b983dee4da18b3ed065972e9e2b
princegobah <babynamek@gmail.com>
2021-12-22 22:35:53 -0500

    Dobe

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const pop= document.getElementById("main")
+pop.remove()
+const newHeader=document.createElement("h1")
+newHeader.id="victory"
+newHeader.textContent="YOUR-NAME is the champion"
+document.body.apppend(newHeader)
